719bc8028eb4d2459d4b75bfa420c129ef90d788
ivann <ivan91ncl@gmail.com>
2023-07-20 23:56:41 +0200

    introduzione routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-        <HomePage />
+      <Routes>
+        <Route path='/' Component={HomePage} exact/>
+        <Route path='/hats' Component={HatsPage}/>
+      </Routes>
     </div>
   );
 }
